969781f1b6b8d22d827e33f740065a9673699588
carolinyHbit <caroliny.abreu@healthbit.com.br>
2023-11-14 09:32:08 -0300

    fixed some errors

--- pandas/cap5-exercicio.py
+++ pandas/cap5-exercicio.py
@@ -1,9 +1,9 @@
 import pandas as pd
 
-def limiter():
+def delimiter():
     print('---------------------------------')
 
-paises = pd.read_csv('arquivos/paises.csv', delimiter=';')
+paises = pd.read_csv('arquivos/paises.csv', dedelimiter=';')
 
 # Questão 1 a)
 print(f'Questão 1 - letra a)')
@@ -14,19 +14,19 @@ paises['Region'] = paises['Region'].str.strip()
 # Filtrando os países da Oceania
 paisesOceania = paises[paises['Region'] == 'OCEANIA']['Country']
 
-# Exibindo os países da Oceania
 print(paisesOceania)
 
-limiter()
+delimiter()
 
 # Questão 1 b)
 print(f'Questão 1 - letra b)')
 
 # Contando o número de países da Oceania
 num_paisesOceania = paises['Region'].str.contains('OCEANIA').sum()
+
 print(f"Total de países da Oceania: {num_paisesOceania}")
 
-limiter()
+delimiter()
 
 # Questão 2
 print(f'Questão 2')
@@ -34,10 +34,9 @@ print(f'Questão 2')
 # Calculando a média da taxa de alfabetização
 mediaAlfabetizacao = paises['Literacy (%)'].mean()
 
-# Exibindo a média
 print(f'Média da taxa de alfabetização: {mediaAlfabetizacao:.2f}%')
 
-limiter()
+delimiter()
 
 # Questão 3
 print(f'Questão 3')
@@ -48,10 +47,9 @@ indiceMax = paises['Population'].idxmax()
 # Obtendo o nome e a região do país com a maior população
 paisMaiorPop = paises.loc[indiceMax, ['Country', 'Region']]
 
-# Exibindo o resultado
 print(f'País com a maior população:\n{paisMaiorPop}')
 
-limiter()
+delimiter()
 
 # Questão 4
 print(f'Questão 4')
@@ -63,8 +61,6 @@ paisesSemCosta = paises[paises['Coastline (coast/area ratio)'] == 0]
 caminhoArq = 'arquivos/noCoast.csv'
 paisesSemCosta.to_csv(caminhoArq, index=False)
 
-# Exibindo os países sem costa
 print(paisesSemCosta[['Country', 'Coastline (coast/area ratio)']])
 
-# Informando sobre o salvamento do novo arquivo
-print(f'\nOs países sem costa foram salvos em: {caminhoArq}')
+# print(f'\nOs países sem costa foram salvos em: {caminhoArq}')
